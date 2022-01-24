1d48191c7dc0522861b1a4ceaceb136260f7dc60
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2022-01-24 23:55:42 +0300

    Update programm1.pas

--- programm1.pas
+++ programm1.pas
@@ -1,26 +1,20 @@
-program programm1;
-const n : integer =4;
-const k : integer =5;
+program matstat;
 
+const n   =10;
+const k   =9;
 
 var
    {Массивы координата(время), учитывай k - количество точек и n кол-во измерений}
+   time,x: array [0..k] of real; {k}
+   eps, t_upr:real;
+   err_norm, t_upr_arr, err_upr:array [0..n] of real; {n}
+   i,j:integer;
+   x_zas, time_zas,err_num: array[0..100-2] of real;
    f:text;
-   time,x: array [0..5] of real;{k}
-   diff_x_arr,x_predict_arr,x_exact_arr, diff_x_arr_upr : array [0..4] of real; {n}
-   i,j,v: integer;
-   eps,difference_x, disperssion_x,suma, t_upr, help:real;
-   t_upr_arr:array [0..10] of real;
-   {для построения графика ошибки от числа засечек}
-   x_zas: array of real;
-   t_zas: array of real;
    {массив, содержащий 2 коэффа b_0 и b_1}
    solution:array [0..1] of real;
-
-  
-{------------------------------------------------------------power        --}
-
-            Function power (os: real; st: integer):real;
+   
+Function power (os: real; st: integer):real;
             {Возведение вещественного числа   в  целочисленную  степень.}
                      begin
                        if st < 0 then
@@ -30,8 +24,8 @@ var
                             power:=os*power(os,st-1)
                          else
                             power:=1;
-                     end; {функции power}
-Function Powd(a,b: real): real;
+                     end; {функции power} 
+ Function Powd(a,b: real): real;
             {Возведение вещественного числа  в вещественную степень.}
                       begin
                         if a > 0 then
@@ -56,12 +50,11 @@ Function Powd(a,b: real): real;
                              a:=a+aa;
                            end;
                          Dahop:=(a-6)/0.6745;
-                       end; {function DAHOR} }
-
+                       end;  }
                    var
                    ra:real;
                    begin
-                   ra:=exp((-1/2)*(power(random,2)))/sqrt(2*pi);
+                   ra:=abs(exp((-1/2)*(power(random,2)))/sqrt(2*pi));
                    end;
 
 {фунция МНК, прямая}
@@ -106,21 +99,23 @@ function MNK(var coord,time,solution: array of real): boolean;
       solution[1]:=b_1;
 end;
 Function x_exact(t: real): real;
-
-            {зависимость координаты от времени- прямая.}
                       begin
-                        result:=25+45*t;
+                        result:=20+45*t;
                       end;
 Function x_predict(b_0,b_1,x_p:real):real;
-         var 
-         pred:real;
+          var
+             pred:real;
          begin
          pred:=b_0+b_1*x_p;
          end;
 
-{Головная программа!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!}
-{пока просто наполняем массивы координаты и времени}
+
 begin
+assign(f,'data.csv');
+rewrite(f);
+writeln(f,'Ошибка от возмущения');
+
+{Ошибка от возмущения}
 t_upr:=1;
 for i:=0 to n do
 begin
@@ -132,60 +127,56 @@ time[j]:=j;
 end;
 MNK(x,time,solution);
 {writeln('y=',solution[0],'+',solution[1],'*x');}
- {массив ошибок}
-diff_x_arr[i]:=abs(x_exact(k+t_upr)-x_predict(solution[0],solution[1],k+t_upr));
-{массив предиктов}
-x_predict_arr[i]:=x_predict(solution[0],solution[1],k+t_upr);
-{массив точных решений}
-x_exact_arr[i]:=x_exact(k+t_upr);
-end;
-suma:=0;
-disperssion_x:=0;
-   for v:=0 to n Do{цикл для среднего}
-   begin
-    suma:=suma+x_predict_arr[v];
-     end;
-suma:=suma/(n+1);{среднее}
+err_norm[i]:=abs(x_exact(k+t_upr)-x_predict(solution[0],solution[1],k+t_upr));
 
-for v:=0 to n do{цикл для sum(dx/n)}
-begin
-disperssion_x:=disperssion_x+power(x_predict_arr[v]-suma,1);
-end;{цикл для sum(dx/n)}
-disperssion_x:=disperssion_x/(length(x_predict_arr));
-
-
-{Запись значений ошибки в файл csv}
-assign(f,'data.csv');
-rewrite(f);
-writeln(f,'Ошибка от возмущения');
-for i:=0 to length(diff_x_arr) do
-begin
-writeln(f, diff_x_arr[i]);
+writeln(f,err_norm[i]);
 end;
 
-
-{зависимость ошибки от t_upr}
-{assign(f,'data.csv'); }
-help:=DAHOP();
-writeln(f,'ошибка от упредительного времени', help);
-
+writeln(f,'время упреждения');
+{Ошибка от время упреждения }
+eps:=DAHOP();
+t_upr:=1;
 for j:=0 to k do
 begin
-x[j]:=x_exact(j)+help;
+x[j]:=x_exact(j)+eps;
 time[j]:=j;
 end;
-for i:=0 to length(t_upr_arr) do
+for i:=0 to n do
       begin
          t_upr:=t_upr+i;
          t_upr_arr[i]:=t_upr;
          MNK(x,time,solution);
-       {writeln('y=',solution[0],'+',solution[1],'*x');}
- {массив ошибок}
-diff_x_arr_upr[i]:=abs(x_exact(k+t_upr)-x_predict(solution[0],solution[1],k+t_upr));
-
-writeln(f, diff_x_arr_upr[i], t_upr);
+err_upr[i]:=abs(x_exact(k+t_upr)-x_predict(solution[0],solution[1],k+t_upr));
+writeln(f, t_upr_arr[i]);
+end;
+writeln(f,'Ошибка от время упреждения при eps=', eps);
+for i:=0 to n do
+begin
+writeln(f,err_upr[i]);
 end;
 
-writeln(f,'ошибка от числа засечек', help);
+
+
+writeln(f,'Ошибка от числа засечек');
+{Ошибка от числа засечек}
+eps:=DAHOP();
+t_upr:=1;
+for i:=2 to 100 do
+begin
+for j:=0 to i do
+begin
+   time_zas[j]:=j;
+   x_zas[j]:=x_exact(j)+eps;
+end;
+MNK(x_zas,time_zas,solution);
+err_num[i-2]:=abs(x_exact(i+t_upr)-x_predict(solution[0],solution[1],i+t_upr));
+
+writeln(f,i);
+end;
+writeln(f,'Ошибка от числа засечек при eps=',eps,'t_upr',t_upr);
+for i:=0 to 100-3 do
+begin
+writeln(f,err_num[i]);
+end;
 close(f);
-end.   
+end.
